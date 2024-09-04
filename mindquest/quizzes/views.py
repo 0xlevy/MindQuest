d820c7445c632868d75fb6f54e59c5645ddfcee9
levywanke <levywanke@gmail.com>
2024-09-04 20:49:23 +0300

    mindquest

--- mindquest/quizzes/views.py
+++ mindquest/quizzes/views.py
@@ -17,11 +17,13 @@ def quiz_list(request):
 def quiz_detail(request, quiz_id):
     quiz = get_object_or_404(Quiz, id=quiz_id)
     questions = quiz.questions.all()
+    score = None
+    percentage = None
+    total_questions = questions.count()
 
     if request.method == 'POST':
         # Process the quiz answers
         score = 0
-        total_questions = 0
 
         for question in questions:
             selected_answer_id = request.POST.get(f'question_{question.id}')
@@ -29,15 +31,22 @@ def quiz_detail(request, quiz_id):
                 selected_answer = get_object_or_404(Answer, id=selected_answer_id)
                 if selected_answer.is_correct:
                     score += 1
-                total_questions += 1
 
-        # Provide feedback
-        percentage = (score / total_questions) * 100 if total_questions > 0 else 0
+        # Calculate percentage
+        if total_questions > 0:
+            percentage = (score / total_questions) * 100
+
+        # Add a message to be displayed on the quiz page
         messages.success(request, f'You scored {score} out of {total_questions} ({percentage:.2f}%)')
 
-        return redirect('quiz_detail', quiz_id=quiz.id)
+    return render(request, 'quizzes/quiz_detail.html', {
+        'quiz': quiz,
+        'questions': questions,
+        'score': score,
+        'percentage': percentage,
+        'total_questions': total_questions,
+    })
 
-    return render(request, 'quizzes/quiz_detail.html', {'quiz': quiz, 'questions': questions})
 
 def register(request):
     if request.method == 'POST':
